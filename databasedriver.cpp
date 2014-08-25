15f830c346678a7b94f0801b21df054776f94274
PeterGodfrey <peter.godfrey11@gmail.com>
2014-08-25 13:27:34 +1000

    Update SQL query layout

--- databasedriver.cpp
+++ databasedriver.cpp
@@ -18,9 +18,12 @@ DatabaseDriver::~DatabaseDriver() {
 
 QVector< QPair<QString, int> >DatabaseDriver::getBuildings(void) {
     QSqlQuery q;
-    QString   s;
-    s = QString("select name, id from Buildings;");
-    q.exec(s);
+    QString   t;
+    t = QString(
+        "SELECT name, id "
+        "FROM   Buildings;"
+        );
+    q.exec(t);
     QVector< QPair<QString, int> > v;
     while ( q.next() ) {
         v.append( QPair<QString, int>(q.value(0).toString(), q.value(1).toInt() ) );
@@ -30,9 +33,13 @@ QVector< QPair<QString, int> >DatabaseDriver::getBuildings(void) {
 
 QStringList DatabaseDriver::getFloorplanNames(int building) {
     QSqlQuery q;
-    QString   s;
-    s = QString("select name from Floorplans where building = %1;").arg(building);
-    q.exec(s);
+    QString   t;
+    t = QString(
+        "SELECT name "
+        "FROM   Floorplans "
+        "WHERE  building = %1;"
+        ).arg(building);
+    q.exec(t);
     QStringList l;
     while ( q.next() ) {
         l.append( q.value(0).toString() );
@@ -44,15 +51,25 @@ QStringList DatabaseDriver::getFloorplanNames(int building) {
 void DatabaseDriver::addSample(Sample s) {
     QSqlQuery q;
     QString   t;
-    t = QString("select horizontalIntensity, verticalIntensity from Samples where floorPlan = %1 AND x = %2 AND y = %3;").arg(s.floorPlanID).arg(s.x).arg(s.y);
+    t = QString(
+        "SELECT horizontalIntensity, verticalIntensity "
+        "FROM   Samples "
+        "WHERE  floorPlan = %1 AND x = %2 AND y = %3;"
+        ).arg(s.floorPlanID).arg(s.x).arg(s.y);
     q.exec(t);
     if ( q.next() ) { // Record already exists
         s.horizontalIntensity = (q.value(0).toDouble() + s.horizontalIntensity) / 2; // Take average
         s.verticalIntensity   = (q.value(1).toDouble() + s.verticalIntensity)   / 2;
-        t = QString("update Samples set horizontalIntensity = %1, verticalIntensity = %2 where floorPlan = %3 AND x = %4 AND y = %5;").arg(s.floorPlanID).arg(s.horizontalIntensity).arg(s.verticalIntensity).arg(s.x).arg(s.y);
+        t = QString(
+            "UPDATE Samples "
+            "SET    horizontalIntensity = %1, verticalIntensity = %2 "
+            "WHERE  floorPlan = %3 AND x = %4 AND y = %5;"
+            ).arg(s.floorPlanID).arg(s.horizontalIntensity).arg(s.verticalIntensity).arg(s.x).arg(s.y);
     } else {          // Record does not already exist
-        t = QString("insert into Samples (floorPlan, x, y, verticalIntensity, horizontalIntensity) values (%1, %2, %3, %4, %5);").arg(s.floorPlanID).arg(s.x).arg(s.y).arg(s.verticalIntensity).arg(s.horizontalIntensity);
-
+        t = QString(
+            "INSERT INTO Samples (floorPlan, x, y, verticalIntensity, horizontalIntensity) "
+            "VALUES (%1, %2, %3, %4, %5);"
+             ).arg(s.floorPlanID).arg(s.x).arg(s.y).arg(s.verticalIntensity).arg(s.horizontalIntensity);
     }
     if ( q.exec(t) ) {
         qDebug() << "addSample Success";
